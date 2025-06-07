7261c32618c03c6e3510be7ddc3422efa698e4ee
nandana1311 <164927105+nandana1311@users.noreply.github.com>
2025-06-07 13:35:38 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+  <script>
+    document.querySelector("button").addEventListener("click", function () {
+      const name = document.querySelector("#characterName").value;
+      const place = document.querySelector("#place").value;
+      const story = `Once upon a time, ${name} went to ${place} and had a great adventure!`;
+      document.getElementById("story").textContent = story;
+    });
+  </script>
